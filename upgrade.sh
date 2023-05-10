046d935e72f023d40e5a4151a3e16958c62b8e35
Stefan Andersson <stefan.andersson@hiberworld.com>
2023-05-10 09:11:04 +0200

    Updated root

---
+++ upgrade.sh
@@ -0,0 +1,5 @@
+yarn add @hiber3d/hdk-cli@latest
+yarn add @hiber3d/hdk-core@latest
+yarn add @hiber3d/hdk-utils@latest
+yarn add @hiber3d/hdk-react@latest
+yarn add @hiber3d/hdk-react-components@latest
