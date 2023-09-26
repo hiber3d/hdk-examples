673b64e310fd68d41f03caa0182f1783159d8b52
Stefan Andersson <stefan.andersson@hiberworld.com>
2023-09-26 13:42:56 +0200

    Refreshed packages

---
+++ scripts/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Function to execute commands in the given folder
+update_folder() {
+  (
+    cd "$1" || exit 1
+    echo "Starting update in folder: $1"
+    npx update-by-scope @hiber3d
+    yarn
+    yarn dev
+    echo "Completed update in folder: $1"
+  ) > "$1/update.log" 2>&1 &
+}
+
+# If no arguments are provided, search for subfolders containing package.json
+if [ "$#" -eq 0 ]; then
+  find . -name 'node_modules' -prune -o -name 'package.json' -exec dirname {} \; | while read -r dir; do
+    bash "$0" "$dir"
+  done
+  wait # Wait for all background jobs to finish
+else
+  # Argument provided, treat it as a folder and execute commands
+  update_folder "$1"
+fi
